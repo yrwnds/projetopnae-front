01010073b9b554ccb4e2cd2cea41f5ccee422ffc
yrwnds <l.zanini.nunes@gmail.com>
2026-05-24 08:51:49 -0300

    implementação calendario

--- src/modules/home/dashboard-component/dashboard-component.ts
+++ src/modules/home/dashboard-component/dashboard-component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { FullCalendarModule } from '@fullcalendar/angular';
-import {CalendarOptions, EventSourceInput} from '@fullcalendar/core'; // useful for typechecking
+import {CalendarOptions, EventClickArg, EventSourceInput} from '@fullcalendar/core'; // useful for typechecking
 import dayGridPlugin from '@fullcalendar/daygrid';
-import interactionPlugin, { DateClickArg } from '@fullcalendar/interaction';
+import interactionPlugin, {DateClickArg} from '@fullcalendar/interaction';
 import {EntregaService} from '../../../core/services/entrega-service';
 import {Entrega} from '../../../core/models/entrega';
 import moment from 'moment';
+import {UsuarioService} from '../../../core/services/usuario-service';
+import {AuthService} from '../../../core/services/auth-service';
+import {Produtoentrega} from '../../../core/models/produtoentrega';
+import {ProdutoentregaService} from '../../../core/services/produtoentrega-service';
 
 @Component({
   selector: 'app-dashboard-component',
@@ -15,43 +19,98 @@ import moment from 'moment';
 })
 export class DashboardComponent {
 
-  constructor(private entregaService: EntregaService) {
+  constructor(private entregaService: EntregaService, private usuarioService: UsuarioService, private authService: AuthService, private produtoEntregaService : ProdutoentregaService) {
   }
 
-
+  usuLogado = 0
   e: Entrega[] = [];
-  eventos: { start: string; title: string }[] = []
 
+  entrega: Entrega = {} as Entrega
+  produtos: Produtoentrega[] = []
+  openDetalhes = false
+
+  errorMessage = ''
+  successMessage = ''
 
   ngOnInit() {
+    this.usuarioService.buscarPorEmail(this.authService.getUserEmail()).subscribe(
+      {
+        next: (usu) => {
+          this.usuLogado = usu.id;
+        }
+      }
+    )
     this.entregaService.getAll().subscribe(
       {
         next: (e) => {
           this.e = e;
           console.log(this.e)
+          this.calendarOptions = {
+            events: this.e.map((e) => {
+              if (e.id === this.usuLogado) {
+                return ({
+                  id: e.id.toString(),
+                  title: e.edital.nome,
+                  date: moment.utc(e.dataentrega).format("YYYY-MM-DD")
+                })
+              } else {
+                return ({title: '', date: ''});
+              }
+            })
+          }
         },
         error: (err) => {
           console.error('Erro ao buscar entregas: ', err);
         }
       }
     )
-    this.eventos = this.e.map(e => ({
-      title: e.edital.nome,
-      start: moment.utc(e.dataentrega).format("yyyy-mm-dd")
-    }))
-    console.log(this.eventos)
   }
 
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
     plugins: [dayGridPlugin, interactionPlugin],
-    dateClick: (arg) => this.handleDateClick(arg),
-    events: [this.eventos]
+    eventClick: (arg) => this.handleEventClick(arg),
+    events: []
   };
 
-  handleDateClick(arg: DateClickArg) {
-    alert('date click! ' + arg.dateStr)
+  handleEventClick(arg: EventClickArg){
+    this.entregaService.buscarPorId(arg.event.id).subscribe(
+      {
+        next: (e) => {
+          this.entrega = e;
+    },
+        error: (err) => {
+          console.log("Erro ao buscar entrega no calendário: ", err)
+          this.errorMessage = "Erro. " + JSON.stringify(err.error, ['message']);
+        }
+      }
+    )
+    this.produtoEntregaService.buscarPorIdEntrega(Number(arg.event.id)).subscribe(
+      {
+        next: (p) => {
+          this.produtos = p;
+        },
+        error: (err) => {
+          console.log("Erro ao buscar produtos no calendário: ", err)
+          this.errorMessage = "Erro. " + JSON.stringify(err.error, ['message']);
+        }
+      }
+    )
+    this.openDetalhes = true
   }
+
+  clear(){
+    this.errorMessage = ''
+    this.successMessage = ''
+  }
+
+  fechar(){
+    this.entrega = {} as Entrega
+    this.produtos = []
+    this.openDetalhes = false
+  }
+
+  protected readonly open = open;
 }
 
 
